d8e865058ff5582d9c62e9ae00600d742dc5f45f
Janis Lesinskis <janis@pythoncharmers.com>
2019-05-06 17:01:36 -0400

    Fix typo in CLI

--- src/gilbert/cli.py
+++ src/gilbert/cli.py
@@ -5,7 +5,7 @@ from .site import Site
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='gilbert', description="Gilber static site generator")
+    parser = argparse.ArgumentParser(prog='gilbert', description="Gilbert static site generator")
     parser.add_argument('--root', '-r', type=Path, default=Path.cwd(),
                         help="Root of site data [defaults to CWD]")
     parser.add_argument('action', choices=['init', 'render', 'watch', 'plugins'])
