a21aef8d52ce7b4441d07d0b67129ac6979b753b
Curtis Maloney <curtis@tinbrain.net>
2019-05-07 22:32:31 +1000

    Fix oneshot class

--- src/gilbert/utils.py
+++ src/gilbert/utils.py
@@ -3,11 +3,11 @@ class oneshot:
     def __init__(self, func):
         self.func = func
 
-    def __set_name__(self, name):
+    def __set_name__(self, owner, name):
         self.name = name
 
     def __get__(self, instance, owner):
         if instance is None:
             return self
-        instance.__dict__[name] = value = self.func(instance)
+        instance.__dict__[self.name] = value = self.func(instance)
         return value
