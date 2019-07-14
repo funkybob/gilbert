6e9d07e8a32499cdf27c87cce90443fcac3d7f00
Curtis Maloney <curtis@tinbrain.net>
2019-07-14 12:00:35 +1000

    Update version tags in docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Curtis Maloney'
 author = 'Curtis Maloney'
 
 # The short X.Y version
-version = ''
+version = '0.3'
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.3.4'
 
 
 # -- General configuration ---------------------------------------------------
